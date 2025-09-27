33b231788f751657ba129e84171731e55d0639bd
Fidele012 <f.ndihokubw1@alustudent.com>
2025-09-27 20:00:04 +0000

    Write the function def model(Data_train, Data_valid, layers, activations, alpha=0.001, beta1=0.9, beta2=0.999, epsilon=1e-8, decay_rate=1, batch_size=32, epochs=5, save_path='/tmp/model.ckpt')

--- supervised_learning/optimization/15-model.py
+++ supervised_learning/optimization/15-model.py
@@ -273,4 +273,15 @@ def model(
                         x_minbatch = Xs[start:end]
                         y_minbatch = Ys[start:end]
 
+                    feed_mini = {x: x_minbatch, y: y_minbatch}
+                    sess.run(train_op, feed_dict=feed_mini)
 
+                    if ((step_number + 1) % 100 == 0) and (step_number != 0):
+                        step_cost = sess.run(loss, feed_dict=feed_mini)
+                        step_accuracy = sess.run(accuracy, feed_dict=feed_mini)
+                        print("\tStep {}:".format(step_number + 1))
+                        print("\t\tCost: {}".format(step_cost))
+                        print("\t\tAccuracy: {}".format(step_accuracy))
+            sess.run(tf.assign(global_step, global_step + 1))
+            save_path = saver.save(sess, save_path)
+    return save_path
